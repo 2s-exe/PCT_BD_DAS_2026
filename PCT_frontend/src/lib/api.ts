bc061e8e9e7328c1176d8d7edc46a027d062f88c
2s-exe <saidou.sow@uvci.edu.ci>
2026-06-13 15:30:43 +0000

    fix: add fallback URL for NEXT_PUBLIC_API_URL

--- PCT_frontend/src/lib/api.ts
+++ PCT_frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (process.env.NEXT_PUBLIC_API_URL ?? "").replace(/\/$/, ""),
+  baseURL: (process.env.NEXT_PUBLIC_API_URL ?? "https://pct-bd-das-2026.onrender.com").replace(/\/$/, ""),
   headers: { "Content-Type": "application/json", Accept: "application/json" },
 });
 
